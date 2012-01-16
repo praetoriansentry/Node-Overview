d65fa007a3f0ebfedd8a2aa18a5ab583ce183a4a
John Hilliard <praetoriansentry@gmail.com>
2012-01-16 16:11:55 -0500

    Chat interaction seems a little more realistic now

--- script.js
+++ script.js
@@ -23,6 +23,16 @@ if (!com) {
         $('.logininfo button').click(function () {
             that.setUserName.call(that);
         });
+        $('#username').keyup(function (evt) {
+            if (evt.which === 13) {
+                that.setUserName.call(that);
+            }
+        });
+        $('.chat textarea').keyup(function (evt) {
+            if (evt.which === 13 && !evt.shiftKey) {
+                that.sendMessage.call(that);
+            }
+        });
     };
 
     m.setUserName = function () {
@@ -35,6 +45,56 @@ if (!com) {
         this.username = uname;
         $('.logininfo').addClass('hidden');
         $('.chat').removeClass('hidden');
+        $('.chat textarea').focus();
+    };
+
+    m.sendMessage = function () {
+        var msg = this.clearTextArea(),
+            msgLine = this.createMsgLine(this.username, new Date(), msg);
+        $(msgLine).appendTo('.conversation');
+        this.scrollDown();
+    };
+
+    m.scrollDown = function () {
+        var conversation = $('.conversation'),
+            height = conversation.prop('scrollHeight') - conversation.innerHeight();
+        if (height > 0) {
+            conversation.scrollTop(height);
+        }
+    };
+
+    m.clearTextArea = function () {
+        var currentValue = $('.chat textarea').val();
+        $('.chat textarea').val('');
+        return currentValue;
+    };
+
+    m.createMsgLine = function (username, time, msg) {
+        var div = document.createElement('div'),
+            uname = document.createElement('span'),
+            timestamp = document.createElement('span'),
+            message = document.createElement('span'),
+            mins = time.getMinutes(),
+            hours = time.getHours();
+        if (hours < 10) {
+            hours = '0' + hours;
+        }
+        if (mins < 10) {
+            mins = '0' + mins;
+        }
+        uname.innerHTML = username + ': ';
+        timestamp.innerHTML = '[' + hours + ':' + mins + '] ';
+        message.innerHTML = this.encode(msg);
+
+        div.appendChild(timestamp);
+        div.appendChild(uname);
+        div.appendChild(message);
+
+        return div;
+    };
+
+    m.encode = function (msg) {
+        return $('<div/>').text(msg).html();
     };
 
     com.Manager = Manager;
