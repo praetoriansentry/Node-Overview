c9723ea7ee33cb3c6250eb2ecc8091e390a68dfc
John Hilliard <praetoriansentry@gmail.com>
2012-01-17 00:34:15 -0500

    adding basic hello world for socket

--- server/server.js
+++ server/server.js
@@ -1,9 +1,19 @@
 //Add the current directory to the list of include paths
 require.paths.unshift('.');
-var http = require('http'),
+var app = require('http').createServer(httpHandler),
+    io = require('socket.io').listen(app),
     router = require('router');
 
-http.createServer(function (req, res) {
+function httpHandler(req, res) {
     var r = new router.Router(req, res);
     r.dispatch();
-}).listen(8000, "0.0.0.0");
+}
+
+app.listen(8000, "0.0.0.0");
+
+io.sockets.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
+});
